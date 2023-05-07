5a5060f2d8318f0cbb39a1472cbc50ba49125c0c
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-05-07 15:21:51 -0600

    Creacion de componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
