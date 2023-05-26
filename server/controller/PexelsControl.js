ea1dcade5785ad1eb8b03cd1eb635a95490b0279
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-05-25 23:06:08 -0600

    falta por revisar similitud del coseno

--- server/controller/PexelsControl.js
+++ server/controller/PexelsControl.js
@@ -1,20 +1,39 @@
-const axios = require('axios');
+const axios = require("axios");
+const use = require("@tensorflow-models/universal-sentence-encoder");
 
 async function getPexelsImages(req, res) {
   try {
+    // Carga el modelo
+    const model = await use.load();
+
+    // Obtiene la representación vectorial de la consulta
     const query = req.params.query;
-    const response = await axios.get(`https://api.pexels.com/v1/search?query=${query}`, {
-      headers: {
-        Authorization: process.env.PEXELS_API_KEY
+    const embeddings = await model.embed(query);
+
+    // Calcula la similitud de coseno entre la representación vectorial de la consulta y las representaciones vectoriales precalculadas
+    // ...
+
+    // Utiliza la información de la similitud de coseno para determinar mejores resultados de búsqueda
+    // ...
+
+    const response = await axios.get(
+      `https://api.pexels.com/v1/search?query=${query}`,
+      {
+        headers: {
+          Authorization: process.env.PEXELS_API_KEY,
+        },
       }
-    });
+    );
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ha ocurrido un error mientras se hacia la solicitud hacia el API Pexels' });
+    res.status(500).json({
+      error:
+        "Ha ocurrido un error mientras se hacia la solicitud hacia el API Pexels",
+    });
   }
 }
 
 module.exports = {
-  getPexelsImages
+  getPexelsImages,
 };
