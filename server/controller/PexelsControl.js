1dbcfff3ba2ad36e3fa7548f0b95c517b22dfc2e
WillSanchez8 <william_as@tesch.edu.mx>
2023-05-26 20:36:44 -0600

    Se quitp momentaneamente tensorflow.js

--- server/controller/PexelsControl.js
+++ server/controller/PexelsControl.js
@@ -1,21 +1,8 @@
 const axios = require("axios");
-const use = require("@tensorflow-models/universal-sentence-encoder");
 
 async function getPexelsImages(req, res) {
   try {
-    // Carga el modelo
-    const model = await use.load();
-
-    // Obtiene la representación vectorial de la consulta
     const query = req.params.query;
-    const embeddings = await model.embed(query);
-
-    // Calcula la similitud de coseno entre la representación vectorial de la consulta y las representaciones vectoriales precalculadas
-    // ...
-
-    // Utiliza la información de la similitud de coseno para determinar mejores resultados de búsqueda
-    // ...
-
     const response = await axios.get(
       `https://api.pexels.com/v1/search?query=${query}`,
       {
