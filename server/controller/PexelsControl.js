770156445c3e8fb9b0ab36972ec6ea27623d001a
WillSanchez8 <william_as@tesch.edu.mx>
2023-05-29 12:01:02 -0600

    Integración de API Google Translate

--- server/controller/PexelsControl.js
+++ server/controller/PexelsControl.js
@@ -1,10 +1,12 @@
-const axios = require("axios");
+const axios = require('axios');
 
 async function getPexelsImages(req, res) {
   try {
     const query = req.params.query;
+    //translateText recibe dos parámetros el texto a traducir y el idioma al que se quiere traducir
+    const translatedQuery = await translateText(query, 'en');
     const response = await axios.get(
-      `https://api.pexels.com/v1/search?query=${query}`,
+      `https://api.pexels.com/v1/search?query=${translatedQuery}`,
       {
         headers: {
           Authorization: process.env.PEXELS_API_KEY,
@@ -16,8 +18,24 @@ async function getPexelsImages(req, res) {
     console.error(error);
     res.status(500).json({
       error:
-        "Ha ocurrido un error mientras se hacia la solicitud hacia el API Pexels",
+        'Ha ocurrido un error mientras se hacia la solicitud hacia el API Pexels',
     });
+  }
+}
+
+async function translateText(text, targetLanguage) {
+  try {
+    const response = await axios.post(
+      `https://translation.googleapis.com/v3beta1/projects/axolotl-tescha:translateText?key=${process.env.GOOGLE_TRANSLATE_API_KEY}`,
+      {
+        contents: [text],
+        targetLanguageCode: targetLanguage,
+      }
+    );
+    return response.data.translations[0].translatedText;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
 }
 
