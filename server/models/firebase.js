e8bbe543d6c7ea969de592abbca71fbfafd06a0f
WillSanchez8 <william_as@tesch.edu.mx>
2023-06-06 23:21:24 -0600

    remplanzando archivos de firebase

---
+++ server/models/firebase.js
@@ -0,0 +1,8 @@
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
