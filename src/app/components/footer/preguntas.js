ed5f6bc33f3caf9c68edd69626b8acc3690bb84a
Rene Reyes Cañeda <rene_rc@tesch.edu.mx>
2023-06-15 12:24:18 -0600

    no se que hacer  jaja

--- src/app/components/footer/preguntas.js
+++ src/app/components/footer/preguntas.js
@@ -1,6 +1,15 @@
 const pregunta=document.querySelectorAll('.pregunta .contenedor');
 pregunta.forEach((pregunta)=>{
     pregunta.addEventListener('click',(e)=>{
-        e.currentTarget.classList.toggle('activa')
+        e.currentTarget.classList.toggle('activa');
+
+        const respuesta= pregunta.querySelector('.respuesta');
+        const alturaRealRespuesta = respuesta.scrollHeight;
+
+        if(respuesta.style.maxHeight){
+            console.log('No esta vacia')
+        }else{
+            console.log('esta vacia')
+        }
     })
 });
