f76a2e8fb8cc89ac23edc8d5cef50a6c6d7aeba5
SERGIO AUGUSTO RAYON CORNEJO <sergio_rc@tesch.edu.mx>
2023-06-22 15:07:28 -0600

    asistente listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Axolotl';
+
+  desactivarBlur() {
+    const mainBlurElement = document.querySelector('.main-blur') as HTMLElement;
+    if (mainBlurElement) {
+      mainBlurElement.style.filter = 'none';
+    }
+  }
 }
